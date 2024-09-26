a17edc0bf01a57ed159186071f5ac7a7e3afdb60
Omar <nyirurugoomar@gmail.com>
2024-09-26 17:32:53 +0200

    get and post api for category

--- lib/modals/category.ts
+++ lib/modals/category.ts
@@ -2,8 +2,9 @@ import { Schema, model, models } from "mongoose";
 
 const CategorySchema = new Schema(
     {
-        name: { type: String, required: true },
-        description: { type: String }
+        title:{type:"string", required:true},
+        user: {type:Schema.Types.ObjectId, ref: "User"}
+        
     },
     { timestamps: true }
 );
